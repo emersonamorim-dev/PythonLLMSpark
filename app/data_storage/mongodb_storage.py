651a5d4750efc982e1fb5352e041c99a1a07f9fe
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-08-06 11:08:36 -0300

    PythonLLMSpark

--- app/data_storage/mongodb_storage.py
+++ app/data_storage/mongodb_storage.py
@@ -2,8 +2,8 @@ import logging
 
 def store_data(df, mongodb_uri):
     try:
-        # Escreva os dados transformados no MongoDB
+        # Escreve dados transformados no MongoDB
         df.write.format("mongo").option("uri", mongodb_uri).mode("append").save()
-        logging.info(f"Data successfully written to MongoDB at {mongodb_uri}")
+        logging.info(f"Dados gravados com sucesso no MongoDB em {mongodb_uri}")
     except Exception as e:
-        logging.error(f"Failed to write data to MongoDB at {mongodb_uri}: {e}")
+        logging.error(f"Falha ao gravar dados no MongoDB em {mongodb_uri}: {e}")
